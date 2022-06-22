061e45733711897a19b3eb9b13f7e5464fa96e77
Aman Kumar Singh <amans95btechaiml2025@kccitm.edu.in>
2022-06-22 15:31:22 +0530

    add

---
+++ add_integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter the vlue of  a ");
+    scanf("%d", &a);
+
+    printf("Enter the value of b ");
+    scanf("%d", &b);
+
+    printf(" The sum of a and b id %d", a+ b);
+    return 0;
+}
